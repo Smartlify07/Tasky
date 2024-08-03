d11bf94721d15904cb34b91b0e74e1ad2a905c88
Obinna Anosike <smartlify09@gmail.com>
2024-08-03 23:26:33 +0100

    Added readme file

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,7 +5,7 @@ import HeroSection from "./ui/home/hero";
 export default function Home() {
   return (
     <>
-      <main class="relative min-h-screen w-full bg-white flex flex-col items-center justify-center bg-gradient-to-tr">
+      <main class="relative min-h-screen w-full bg-white flex flex-col items-center justify-center bg-gradient-to-tl">
         <Gradient />
         <div className="container mx-auto z-10 md:w-11/12 lg:w-10/12">
           <HeroSection />
