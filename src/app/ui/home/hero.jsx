d5f765bb91b444cf46c36739a5582bea5448eb4d
Obinna Anosike <smartlify09@gmail.com>
2024-08-03 23:17:56 +0100

    Added readme file

--- src/app/ui/home/hero.jsx
+++ src/app/ui/home/hero.jsx
@@ -1,8 +1,10 @@
+import Image from "next/image";
+
 export default function HeroSection() {
   return (
     <section className="flex flex-col items-center gap-5">
       <h1 className="text-5xl md:text-7xl text-center font-medium text-dark">
-        Tasky
+        TaskBud
       </h1>
       <h2 className="text-2xl text-center md:w-7/12 md:text-2xl text-tertiary">
         Track tasks, <span className="text-dark font-medium">collaborate</span>,
@@ -18,6 +20,15 @@ export default function HeroSection() {
       <button className="bg-[#222] px-7 py-3 text-primary font-medium text-lg rounded-md transition-colors hover:bg-[#111]">
         Get Started Now
       </button>
+
+      <div className="md:w-9/12 mt-5 flex items-center flex-col mx-auto border">
+        <Image
+          width={500}
+          height={500}
+          src={"/assets/images/hero/hero.png"}
+          alt="app-demo"
+        />
+      </div>
     </section>
   );
 }
