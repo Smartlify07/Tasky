7fb8dbad485ef3c91cbf3b1ceda54173e2593936
Obinna Anosike <smartlify09@gmail.com>
2024-08-06 21:01:41 +0100

    Fixing api prod issue

--- src/app/utils/checkEnv.js
+++ src/app/utils/checkEnv.js
@@ -2,7 +2,7 @@ export const checkEnvironment = () => {
   let base_url =
     process.env.NODE_ENV === "development"
       ? "http://localhost:3000"
-      : "https://taskbud-git-feature-homepage-obinna-anosikes-projects.vercel.app/";
+      : "https://taskbud-git-feature-homepage-obinna-anosikes-projects.vercel.app";
 
   return base_url;
 };
